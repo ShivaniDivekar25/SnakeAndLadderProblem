70b54d4cebbbbd08528dc3d2d7a08813bc02c91a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-07 14:25:50 +0530

    Modified to Position Of Player program

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
+            //Taking user input
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:Position of Player");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Switch case to call different methods
+            switch (option)
+            {
+                case 1:
+                    SnakeAndLadder.Position();
+                    break;
+            }
         }
     }
 }
---
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    //Created class Snake And Ladder
+    public class SnakeAndLadder
+    {
+        //Created method position
+        public static void Position()
+        {
+            int position = 0;
+            Console.WriteLine("The position of player start with {0}", position);
+        } 
+    }
+}
